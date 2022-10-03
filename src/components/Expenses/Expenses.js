feed9173edc15ee135888448bf861dd28500a200
unknown <lusinetorosyan.evistep@gmail.com>
2022-10-03 23:14:49 +0400

    equal with 67 video

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,24 +2,31 @@ import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = selectedYear => {
+    console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
 
-
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangedFilter={filterChangeHandler} />
-        {props.items.map(item => (
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangedFilter={filterChangeHandler}
+        />
+        {filteredExpenses.map(item => (
           <ExpenseItem
+            key={item.id}
             title={item.title}
             amount={item.amount}
             date={item.date}
