48598594c81e6513a8729cbad72d57bd4d96ce1d
LusineTorosian <lusinetorosyan.evistep@gmail.com>
2022-10-05 12:35:53 +0400

    finished

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -7,13 +7,13 @@ const ExpensesList = (props) => {
   console.log(props);
 
 
-  if (props.item.length === 0) {
+  if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses.</h2>
   }
 
   return (
     <ul className="expenses-list">
-        {props.item.map((expense) => (
+        {props.items.map((expense) => (
             <ExpenseItem
                 key={expense.id}
                 title={expense.title}
